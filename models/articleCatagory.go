5b174b2622c8214fae83cb34dd01053d544c2499
hippo <binzero@ymail.com>
2016-06-27 12:52:57 +0800

    fix created column

--- models/articleCatagory.go
+++ models/articleCatagory.go
@@ -9,6 +9,6 @@ func GetArticleCatagory(page int64) ([]*ArticleCatagory, error) {
 	awcs := make([]*ArticleCatagory, 0)
 	start := (page - 1) * 10
 	//err := db.Limit(1,1).Sql("select a.*,(select title from catagory where id = a.catagory_id) as cata_name from article a order by created desc").Find(&awcs)
-	err := db.Table("article").Join("left","catagory","article.catagory_id=catagory.id").Desc("Created").Limit(10,int(start)).Find(&awcs)
+	err := db.Table("article").Join("left","catagory","article.catagory_id=catagory.id").Desc("article.Created").Limit(10,int(start)).Find(&awcs)
 	return awcs, err
 }
